4af99f03abe7340c0f628aec274a14f627f6017e
nothing-inside <darinka.golovkova@yandex.ru>
2023-05-10 17:58:45 +0300

    J.S.Домашнее задание.

---
+++ script.js
@@ -0,0 +1,20 @@
+let button = document.getElementById('temp')
+let buttonC = document.getElementById('tempC')
+let buttonF = document.getElementById('tempF')
+let result = document.getElementById('result')
+
+function getTempCelsia(temp) {
+let r = (temp * 9/5) + 32;
+    result.innerHTML = temp+" \xB0F is " + r + " \xB0C"
+}
+function getTempFaregeit(temp) {
+let r = (temp - 32) * 5/9;
+    result.innerHTML = temp+" \xB0F is " + r + " \xB0C"
+}
+buttonC.addEventListener('click', function() {
+  getTempCelsia(temp.value)
+})
+buttonF.addEventListener('click', function() {
+  getTempFaregeit(temp.value)
+})
+
